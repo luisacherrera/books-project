d82354f68ab7e1a66a10dc3de80eb7d56a734968
Luis Herrera <la.herrera457@gmail.com>
2018-02-09 14:58:09 +0100

    added book detail route, modified routes to promises

--- routes/books.js
+++ routes/books.js
@@ -3,22 +3,34 @@ const router = express.Router();
 const Book = require('../models/books.js');
 
 router.get('/', (req, res, next) => {
-  const promise = Book.find();
-
-  promise.then(booksData => {
-    res.render('books/books-list', {booksData});
-  });
-  promise.catch(err => {
-    return next(err);
-  });
-});
-
-router.get('/:id', (req, res, next) => {
-
+  Book.find()
+    .then(booksData => {
+      res.render('books/books-list', {booksData});
+    })
+    .catch(err => {
+      return next(err);
+    });
 });
 
 router.get('/create', (req, res, next) => {
   res.render('books/create-book');
+});
+
+router.get('/:id', (req, res, next) => {
+  const bookId = req.params.id;
+  Book.findById(bookId)
+    .then((result) => {
+      let data = {
+        id: result.id,
+        title: result.title,
+        author: result.author,
+        description: result.description
+      };
+      res.render('books/book-detail', data);
+    })
+    .catch(err => {
+      return next(err);
+    });
 });
 
 router.post('/create', (req, res, next) => {
@@ -30,13 +42,13 @@ router.post('/create', (req, res, next) => {
 
   const newBook = new Book(infoBook);
 
-  newBook.save((err) => {
-    if (err) {
-      return next(err);
-    } else {
+  newBook.save()
+    .then((result) => {
       res.redirect('/books');
-    }
-  });
+    })
+    .catch((err) => {
+      return next(err);
+    });
 });
 
 module.exports = router;
