82c77c5ed1242acf1aa77484e62ca763018c983b
Luis Herrera <la.herrera457@gmail.com>
2018-02-12 10:39:39 +0100

    edit page for books started

--- routes/books.js
+++ routes/books.js
@@ -16,6 +16,42 @@ router.get('/create', (req, res, next) => {
   res.render('books/create-book');
 });
 
+router.get('/:id/edit', (req, res, next) => {
+  // if (!req.session.currentUser) {
+  //   return res.redirect('/auth/login');
+  // }
+  const bookId = req.params.id;
+  Book.findById(bookId)
+    .then((result) => {
+      let data = {
+        id: result.id,
+        title: result.title,
+        author: result.author,
+        description: result.description
+      };
+      res.render('books/edit', data);
+    }).catch(err => {
+      return next(err);
+    });
+});
+
+// router.post('/:id', (req, res, next) => {
+//   const bookId = req.params.id;
+
+//   const updateInfo = {
+//     title: req.body.title,
+//     author: req.body.author,
+//     description: req.body.description
+//   };
+
+//   Celebrity.findByIdAndUpdate(celebId, updateInfo, (err, celebrity) => {
+//     if (err) {
+//       return next(err);
+//     }
+//     return res.redirect('/celebrities');
+//   });
+// });
+
 router.get('/:id', (req, res, next) => {
   const bookId = req.params.id;
   Book.findById(bookId)
