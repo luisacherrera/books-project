0bd7f6b77e12b7f019916c1c2be0e2afe1a79c2e
VladC <ciobanu.vlad@yahoo.com>
2018-02-13 17:10:46 +0100

    search bar working

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,20 @@
 
 const express = require('express');
 const router = express.Router();
+const Book = require('../models/books.js');
 
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.post('/search', (req, res, next) => {
+  Book.find({title: req.body.title})
+    .then(booksData => {
+      res.render('books/books-list', {booksData});
+    })
+    .catch(err => {
+      return next(err);
+    });
 });
 
 module.exports = router;
