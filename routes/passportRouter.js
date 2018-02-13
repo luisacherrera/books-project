aecb910dedfef078d83ffc50a439cf4e3975f79b
VladC <ciobanu.vlad@yahoo.com>
2018-02-13 20:28:32 +0100

    deleted unused var  in passportRouter

--- routes/passportRouter.js
+++ routes/passportRouter.js
@@ -2,13 +2,8 @@
 
 const express = require('express');
 const router = express.Router();
-// User model
-const User = require('../models/user');
-// Bcrypt to encrypt passwords
-const bcrypt = require('bcrypt');
-const bcryptSalt = 10;
+
 const ensureLogin = require('connect-ensure-login');
-const passport = require('passport');
 
 router.get('/', ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render('index', { user: req.user });
